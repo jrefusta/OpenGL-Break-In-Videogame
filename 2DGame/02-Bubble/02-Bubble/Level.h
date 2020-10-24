0b1af3d47c2979c278c202eb20fcd0e8152765f9
jrefusta <47823944+jrefusta@users.noreply.github.com>
2020-10-24 17:19:06 +0200

    Add files via upload

---
+++ 2DGame/02-Bubble/02-Bubble/Level.h
@@ -0,0 +1,44 @@
+#ifndef _SCENE_INCLUDE
+#define _SCENE_INCLUDE
+
+
+#include <glm/glm.hpp>
+#include "ShaderProgram.h"
+#include "Scene.h"
+#include "Game.h"
+#include "TileMap.h"
+#include "Player.h"
+#include "Ball.h"
+
+
+// Scene contains all the entities of our game.
+// It is responsible for updating and render them.
+
+
+class Level : public Scene
+{
+
+public:
+	Level();
+	~Level();
+
+	void init();
+	void update(int deltaTime);
+	void render();
+
+private:
+	void initShaders();
+
+private:
+	TileMap *map;
+	Player* player;
+	Ball* ball;
+	ShaderProgram texProgram;
+	float currentTime;
+	glm::mat4 projection;
+
+};
+
+
+#endif // _SCENE_INCLUDE
+
