e7105ce6e75a73ed516a3d87b62f657bafac3fcc
AlexNunez0 <alexxxnr97@gmail.com>
2020-11-09 03:41:43 +0100

    Cosas

--- 2DGame/02-Bubble/02-Bubble/Player.cpp
+++ 2DGame/02-Bubble/02-Bubble/Player.cpp
@@ -6,36 +6,89 @@
 #include "Game.h"
 
 
-/*enum PlayerAnims
+enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
-};*/
+	                    LOOK_UP_LEFT_2,     LOOK_UP_LEFT_1,     LOOK_UP_CENTER,     LOOK_UP_RIGHT_1,     LOOK_UP_RIGHT_2,
+	LOOK_CENTER_LEFT_3, LOOK_CENTER_LEFT_2, LOOK_CENTER_LEFT_1, LOOK_CENTER_CENTER, LOOK_CENTER_RIGHT_1, LOOK_CENTER_RIGHT_2, LOOK_CENTER_RIGHT_3,
+	LOOK_DOWN_LEFT_3,   LOOK_DOWN_LEFT_2,   LOOK_DOWN_LEFT_1,   LOOK_DOWN_CENTER,   LOOK_DOWN_RIGHT_1,   LOOK_DOWN_RIGHT_2,   LOOK_DOWN_RIGHT_3,
+	CLOSE
+};
 
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram, int ID)
 {
 	if (ID == 4) spritesheet.loadFromFile("images/cop.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	else spritesheet.loadFromFile("images/player.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(19, 26), glm::vec2(1.0, 1.0), &spritesheet, &shaderProgram);
-	/*sprite->setNumberAnimations(4);
+	sprite = Sprite::createSprite(glm::ivec2(19, 26), glm::vec2(1/7.0, 0.25), &spritesheet, &shaderProgram);
+	sprite->setNumberAnimations(20);
 	
-		sprite->setAnimationSpeed(STAND_LEFT, 8);
-		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
+		sprite->setAnimationSpeed(LOOK_UP_LEFT_2, 8);
+		sprite->addKeyframe(LOOK_UP_LEFT_2, glm::vec2(1/7.f, 0.f));
 		
-		sprite->setAnimationSpeed(STAND_RIGHT, 8);
-		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+		sprite->setAnimationSpeed(LOOK_UP_LEFT_1, 8);
+		sprite->addKeyframe(LOOK_UP_LEFT_1, glm::vec2(2/7.f, 0.f));
+
+		sprite->setAnimationSpeed(LOOK_UP_CENTER, 8);
+		sprite->addKeyframe(LOOK_UP_CENTER, glm::vec2(3/7.f, 0.f));
+
+		sprite->setAnimationSpeed(LOOK_UP_RIGHT_1, 8);
+		sprite->addKeyframe(LOOK_UP_RIGHT_1, glm::vec2(4/7.f, 0.f));
+
+		sprite->setAnimationSpeed(LOOK_UP_RIGHT_2, 8);
+		sprite->addKeyframe(LOOK_UP_RIGHT_2, glm::vec2(5/7.f, 0.f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_LEFT_3, 8);
+		sprite->addKeyframe(LOOK_CENTER_LEFT_3, glm::vec2(0.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_LEFT_2, 8);
+		sprite->addKeyframe(LOOK_CENTER_LEFT_2, glm::vec2(1/7.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_LEFT_1, 8);
+		sprite->addKeyframe(LOOK_CENTER_LEFT_1, glm::vec2(2/7.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_CENTER, 8);
+		sprite->addKeyframe(LOOK_CENTER_CENTER, glm::vec2(3/7.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_RIGHT_1, 8);
+		sprite->addKeyframe(LOOK_CENTER_RIGHT_1, glm::vec2(4/7.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_RIGHT_2, 8);
+		sprite->addKeyframe(LOOK_CENTER_RIGHT_2, glm::vec2(5/7.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_CENTER_RIGHT_3, 8);
+		sprite->addKeyframe(LOOK_CENTER_RIGHT_3, glm::vec2(6/7.f, 0.25f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_LEFT_3, 8);
+		sprite->addKeyframe(LOOK_DOWN_LEFT_3, glm::vec2(0.f, 0.5f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_LEFT_2, 8);
+		sprite->addKeyframe(LOOK_DOWN_LEFT_2, glm::vec2(1/7.f, 0.5f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_LEFT_1, 8);
+		sprite->addKeyframe(LOOK_DOWN_LEFT_1, glm::vec2(2/7.f, 0.5f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_CENTER, 8);
+		sprite->addKeyframe(LOOK_DOWN_CENTER, glm::vec2(3/7.f, 0.5f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_RIGHT_1, 8);
+		sprite->addKeyframe(LOOK_DOWN_RIGHT_1, glm::vec2(4/7.f, 0.5f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_RIGHT_2, 8);
+		sprite->addKeyframe(LOOK_DOWN_RIGHT_2, glm::vec2(5/7.f, 0.5f));
+
+		sprite->setAnimationSpeed(LOOK_DOWN_RIGHT_3, 8);
+		sprite->addKeyframe(LOOK_DOWN_RIGHT_3, glm::vec2(6/7.f, 0.5f));
+
+		sprite->setAnimationSpeed(CLOSE, 8);
+		sprite->addKeyframe(CLOSE, glm::vec2(0.f, 0.75f));
+		sprite->addKeyframe(CLOSE, glm::vec2(1/7.f, 0.75f));
+		sprite->addKeyframe(CLOSE, glm::vec2(2/7.f, 0.75f));
+		sprite->addKeyframe(CLOSE, glm::vec2(3/7.f, 0.75f));
+		sprite->addKeyframe(CLOSE, glm::vec2(4/7.f, 0.75f));
+		sprite->addKeyframe(CLOSE, glm::vec2(5/7.f, 0.75f));
+		sprite->addKeyframe(CLOSE, glm::vec2(6/7.f, 0.75f));
 		
-		sprite->setAnimationSpeed(MOVE_LEFT, 8);
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
-		
-		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
-		
-	sprite->changeAnimation(0);*/
+	sprite->changeAnimation(LOOK_DOWN_CENTER);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
 	
